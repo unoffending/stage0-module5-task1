b2aa6c8fe270704bd81142389d235697beecfb8d
unoffending <109032978+unoffending@users.noreply.github.com>
2023-12-25 00:01:16 +0600

    Update ArrayTasks.java

--- src/main/java/com/epam/mjc/stage0/ArrayTasks.java
+++ src/main/java/com/epam/mjc/stage0/ArrayTasks.java
@@ -128,7 +128,5 @@ public class ArrayTasks {
      * arr = [[3, 1, 2,], [3,2]] -> [[2, 3], [1, 2, 3]]
      * arr = [[5, 4], [7]]       -> [[7], [4, 5]]
      */
-    public int[][] sortRaggedArray(int[][] arr) {
-
-    }
+    
 }
